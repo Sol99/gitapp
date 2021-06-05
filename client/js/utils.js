f881c9410340cbedc9f439e2b8c6c01669f7c274
Lautaro <lautyguasch10@gmail.com>
2021-06-05 19:57:02 -0300

    agrega funcion monefy original

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,7 +1,19 @@
 export function monefy(num) {
     if (!num) return '';
+
     const numStr = String(num);
-    return numStr;
+    const points = numStr.length / 3;
+    const result = [];
+
+    for (let i = 0; i < points; i++) {
+        const s = -3 * (i + 1);
+        const e = -3 * i || undefined;
+        const chunk = numStr.slice(s, e);
+
+        result.push(chunk);
+    }
+
+    return result.reverse().join('.');
 }
 
 export function getRandomColor() {
